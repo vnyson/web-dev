e696cd88963d963bd72b3820b9e45d96746c86df
vnyson <vnyson@gmail.com>
2026-05-22 01:58:39 -0400

    fix-issues

--- sites/tennis-stringing/postcss.config.js
+++ sites/tennis-stringing/postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
+    'postcss-url': {
+      url: (asset) => {
+        // Rewrite asset paths to be relative to css/bundle.css
+        if (asset.url.includes('../../assets/fonts') || asset.url.includes('../fonts')) {
+          return asset.url.replace('../../assets/fonts', '../assets/fonts').replace('../fonts', '../assets/fonts');
+        }
+        return asset.url;
+      },
+    },
   },
 };
